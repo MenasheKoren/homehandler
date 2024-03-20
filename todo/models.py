af4814149ee140e57cf8a00e3daf6d780fb64cf6
menashekoren <menashekoren@gmail.com>
2024-03-20 16:58:24 +0200

    Add completed at and created at fields, closing #7

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if self.completed and not self.completed_at:
+            self.completed_at = timezone.now()
+        super().save(*args, **kwargs)
