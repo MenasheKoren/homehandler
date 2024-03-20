50ff354a266b822535c40ba4413e6f747f599d0f
menashekoren <menashekoren@gmail.com>
2024-03-20 15:11:51 +0200

    Update tests to include setup method, referencing #6

--- todo/tests.py
+++ todo/tests.py
@@ -2,12 +2,20 @@ from django.test import TestCase
 from .models import Todo
 
 class TodoModelTests(TestCase):
-    def test_create_todo_basic_attributes(self):
-        todo = Todo.objects.create(
+    def setUp(self) -> None:
+        self.todo1 = Todo.objects.create(
             title='Todo Item 1',
             description='This is the first todo item.',
             completed=False
         )
-        self.assertEqual(todo.title, 'Todo Item 1')
-        self.assertEqual(todo.description, 'This is the first todo item.')
-        self.assertFalse(todo.completed, 'Todo completed should be false by default.')
+        self.todo2 = Todo.objects.create(
+            title='Todo Item 2',
+            description='This is the second todo item.',
+            completed=True
+        )
+        
+    def test_create_todo_basic_attributes(self):
+        self.assertEqual(self.todo1.title, 'Todo Item 1')
+        self.assertEqual(self.todo1.description, 'This is the first todo item.')
+        self.assertFalse(self.todo1.completed)
+        self.assertTrue(self.todo2.completed)
