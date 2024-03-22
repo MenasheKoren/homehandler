9a9210682b3efddd6ffe1a6746fb5156b1159b73
menashekoren <menashekoren@gmail.com>
2024-03-22 15:01:21 +0200

    Add basic tests for validation and error handling, referencing #8

--- homehandler/settings.py
+++ homehandler/settings.py
@@ -136,6 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
 COLORIZE_CONSOLE_OUTPUT = True
 
 DJANGO_BOOTSTRAP5 = {
--- todo/tests/test_forms.py
+++ todo/tests/test_forms.py
@@ -1,18 +1,39 @@
 from django.test import TestCase
 from ..forms import TodoForm
 
-class TestFormTests(TestCase):
+
+class TodoFormTests(TestCase):
     def test_todo_form_has_instance(self):
         form = TodoForm()
         self.assertIsInstance(form, TodoForm)
 
     def test_todo_form_has_fields(self):
         form = TodoForm()
-        self.assertIsNotNone(len(form.fields), 'TodoForm should have at least one field.')
+        self.assertTrue(
+            len(form.fields) > 0, 'TodoForm should have at least one field.')
 
     def test_todo_form_field_presence(self):
         form = TodoForm()
         expected_fields = ['completed', 'title', 'description']
         actual_fields = list(form.fields)
-        self.assertCountEqual(expected_fields, actual_fields, 'Form fields do not match actual fields.')
+        self.assertCountEqual(expected_fields, actual_fields,
+                              'Form fields do not match actual fields.')
 
+    def test_todo_form_data_validation(self):
+        form_data = {
+            'completed': False,
+            'title': 'Todo Form Item 1',
+            'description': 'This is the first todo form item.',
+        }
+        form = TodoForm(data=form_data)
+        self.assertTrue(form.is_valid(), 'Form is not valid.')
+
+    def test_todo_form_error_handling(self):
+        form_data = {
+            'completed': False,
+            'title': '',
+            'description': 'This is the first todo form item.',
+        }
+        form = TodoForm(data=form_data)
+        self.assertFalse(form.is_valid(), 'Form should be invalid')
+        self.assertIn('title', form.errors)
