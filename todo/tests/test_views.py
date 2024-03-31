16452386e9185015284d5bec0e91ef6ee525cc98
menashekoren <menashekoren@gmail.com>
2024-03-31 13:27:50 +0300

    Add list view instance test, referencing #10

--- todo/tests/test_views.py
+++ todo/tests/test_views.py
@@ -2,7 +2,15 @@ from django.test import TestCase
 from django.urls import reverse
 
 class CreateTodoViewTests(TestCase):
-    def test_todo_create_view_has_instance(self):
+    def test_create_todo_view_has_instance(self):
         response = self.client.get(reverse('todo:create'))
         self.assertTrue(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/create_todo.html')
+
+class ListTodoViewTests(TestCase):
+    def test_list_todo_view_has_instance(self):
+        response = self.client.get(reverse('todo:list'))
+        self.assertTrue(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/list_todo.html')
+
+    
