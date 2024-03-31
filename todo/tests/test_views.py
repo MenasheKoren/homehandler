63a9825ec47b9bd517a4a605395dd160e954c7cd
menashekoren <menashekoren@gmail.com>
2024-03-31 14:16:23 +0300

    Add create todo test, referencing #10

--- todo/tests/test_views.py
+++ todo/tests/test_views.py
@@ -6,6 +6,17 @@ class TodoCreateViewTests(TestCase):
         response = self.client.get(reverse('todo:create'))
         self.assertTrue(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/create_todo.html')
+        
+    def test_create_todo_item(self):
+        post_data = {
+            'completed': False,
+            'title': 'New Todo',
+            'description': 'This is a new todo item.'
+        }
+
+        response = self.client.post(reverse('todo:create'), post_data, follow=True)
+
+        self.assertRedirects(response, reverse('todo:list'), status_code=302, target_status_code=200)
 
 class TodoListViewTests(TestCase):
     def test_todo_list_view_has_instance(self):
